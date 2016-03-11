6828a4ca55727fa16d8e3c184c2fd978cea12a68
Vincent Fahrenholz <fahrenholz@strato-rz.de>
2016-03-11 16:27:09 +0100

    file headers populated

--- plib/controllers/ConfigurationController.php
+++ plib/controllers/ConfigurationController.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * This File is a part of the plesk-acronis extension (https://github.com/StratoAG/plesk-acronis)
+ *
+ * Created by Vincent Fahrenholz <fahrenholz@strato-rz.de>
+ *
+ * Date: 11.03.16
+ * Time: 16:25
+ *
+ * Short Info
+ *
+ * @licence http://www.apache.org/licenses/LICENSE-2.0 Apache Licence v. 2.0
+ */
 
 class ConfigurationController extends pm_Controller_Action
 {
