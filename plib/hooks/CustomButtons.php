7c1d8c254c68b6523fb3c2c439e1e724872db57a
Vincent Fahrenholz <fahrenholz@strato-rz.de>
2016-03-13 11:22:11 +0100

    removed install-scripts and added Hook-Class

--- plib/hooks/CustomButtons.php
+++ plib/hooks/CustomButtons.php
@@ -1,13 +1,28 @@
 <?php
+
 /**
  * This File is a part of the plesk-acronis extension (https://github.com/StratoAG/plesk-acronis)
  *
  * Created by Vincent Fahrenholz <fahrenholz@strato-rz.de>
  *
- * Date: 11.03.16
- * Time: 16:25
+ * Date: 13.03.16
+ * Time: 11:21
  *
  * Short Info
  *
  * @licence http://www.apache.org/licenses/LICENSE-2.0 Apache Licence v. 2.0
  */
+
+/**
+ * Class Modules_AcronisBackup_CustomButtons
+ *
+ * Description
+ *
+ * @category Hook
+ * @author   Vincent Fahrenholz <fahrenholz@strato.de>
+ * @version  Release: 1.0.0
+ */
+class Modules_AcronisBackup_CustomButtons
+{
+
+}
