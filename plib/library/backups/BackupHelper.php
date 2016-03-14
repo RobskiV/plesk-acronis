f0a7e4956fbd3878320550f45d4e1afc13b69811
Eitan Schuler <schuler@strato-rz.de>
2016-03-14 12:33:19 +0100

    Implement getRecoveryPoints

--- plib/library/backups/BackupHelper.php
+++ plib/library/backups/BackupHelper.php
@@ -42,12 +42,52 @@ class Modules_AcronisBackup_backups_BackupHelper
         }
 
         $responseArray = json_decode($response['body'],true);
-        
+
         $planNames = [];
         foreach ($responseArray['data'] as $instance) {
             $planNames[] = $instance['name'];
         }
 
         return $planNames;
+    }
+
+    /**
+     * getRecoveryPoints
+     *
+     * Description
+     *
+     *
+     */
+    public static function getRecoveryPoints()
+    {
+        $settings = Modules_AcronisBackup_settings_SettingsHelper::getAccountSettings();
+        if (! isset($settings['password'])) {
+            return;
+        }
+
+        $request = new Modules_AcronisBackup_webapi_Request($settings['host'], $settings['username'], $settings['password']);
+        $machineId = Modules_AcronisBackup_settings_SettingsHelper::getMachineId();
+        $response = $request->request('GET', '/api/ams/resources/' . $machineId . '/recoverypoints');
+
+        if ($response['code'] != 200 || !isset($response['body'])) {
+            throw new Exception('API returned unexpected response');
+        }
+
+        $responseArray = json_decode($response['body'], true);
+        $backupSettings = Modules_AcronisBackup_settings_SettingsHelper::getBackupSettings();
+
+        $recoveryPoints = [];
+        foreach ($responseArray['data'] as $instance) {
+            if ($instance['backupPlan'] === $backupSettings['backupPlan']) {
+                $recoveryPoints[] = array(
+                    'ItemSliceName' => $instance['ItemSliceName'],
+                    'ItemSliceTime' => $instance['ItemSliceTime']
+                );
+            }
+        }
+
+        return $recoveryPoints;
+
+
     }
 }
